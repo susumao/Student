8adbd488b9f9576b0720af2bf932fa7943af8e0c
maol <86232506@qq.com>
2021-04-29 09:34:19 +0800

    枚举

--- smart-service/smart-student/src/main/java/com/smart/controller/StudentController.java
+++ smart-service/smart-student/src/main/java/com/smart/controller/StudentController.java
@@ -1,13 +1,16 @@
 package com.smart.controller;
 
 
+import com.smart.pojo.Day;
 import com.smart.pojo.Student;
 import com.smart.service.StudentService;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.EnumUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.rmi.ServerException;
 import java.util.Objects;
 
 @RestController
@@ -18,8 +21,12 @@ public class StudentController {
     @GetMapping("/query/{id}")
     public Student query(@PathVariable Integer id) throws Exception {
         Student student = studentService.getById(id);
-        if (Objects.isNull(student)) throw new Exception("未找到");
+        if (Objects.isNull(student)) throw new ServerException("未找到");
         return student;
+    }
 
+    public static void main(String[] args) {
+        boolean validEnum = EnumUtils.isValidEnum(Day.class, "MONDAY");
+        System.out.println(validEnum);
     }
 }
